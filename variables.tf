385be5370d092db8158f4647c72902388f649fc4
vipin2060 <vipinvikraman86@gmail.com>
2024-10-21 22:09:23 +0800

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "metal_metro" {}
+variable "metal_project_id" {}
+variable "metal_vxlan" {}
+variable "metal_token" {}
+
+variable "metal_connection_name" {}
+variable "metal_connection_speed" {}
+variable "metal_port_type" {}
+variable "emails" {}
+variable "redundancy_type" {}
+
+variable "metal_hostname" {}
+variable "metal_plan" {}
+variable "metal_os" {}
+variable "metal_billing_cycle" {}
+
+variable "FCR2Metal_Connectionname" {}
+variable "FCR2metal_Speed" {}
+variable "purchase_order_number" {}
+
+variable "metal_bond_type" {}
+
+variable "FCR_Layer3connection_name" {}
+variable "BGPIP_on_FCR" {}
+variable "Metal_ASN" {}
+variable "Metal_Layer3connection_name" {}
+variable "MetalIP_on_FCR" {}
